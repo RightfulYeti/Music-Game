4d8de9d79aa798d718d181050f84ae27cceef5c2
Jeppe Willatzen <JeppeWi@outlook.com>
2020-03-12 20:18:03 +0100

    Refactoring some scripts
    Refactoring some scripts

---
+++ Music-Game/Assets/Scripts/Player/UmbrellaController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UmbrellaController : MonoBehaviour
+{
+    public Camera cam;  //Drag the camera object here
+    Vector3 mouse_pos;
+    public Transform target; //Assign to the object you want to rotate
+    Vector3 object_pos;
+    float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        mouse_pos = Input.mousePosition;
+        mouse_pos.z = 5.23f; //The distance between the camera and object
+        object_pos = Camera.main.WorldToScreenPoint(target.position);
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
