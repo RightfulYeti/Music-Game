dc94f137200bdb82c5cdc46ad02108901c66843f
Adam Olsson <adaols80@gmail.com>
2020-03-13 10:59:15 +0100

    Orbit script for the umbrella
    Quaternion.Euler(0, 0, angle * Time.deltaTime * 5) change the * 5 to change the speed.

--- Music-Game/Assets/Scripts/Player/UmbrellaController.cs
+++ Music-Game/Assets/Scripts/Player/UmbrellaController.cs
@@ -10,6 +10,10 @@ public class UmbrellaController : MonoBehaviour
     Vector3 object_pos;
     float angle;
 
+    Vector3 delta_mouse_pos;
+    Vector3 previous_mouse_pos;
+    Vector3 orbit_mouse_vector;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +24,30 @@ public class UmbrellaController : MonoBehaviour
     {
         mouse_pos = Input.mousePosition;
         mouse_pos.z = 5.23f; //The distance between the camera and object
+
         object_pos = Camera.main.WorldToScreenPoint(target.position);
         mouse_pos.x = mouse_pos.x - object_pos.x;
         mouse_pos.y = mouse_pos.y - object_pos.y;
+
+        orbit_mouse_vector = Input.mousePosition;
+
+        delta_mouse_pos = previous_mouse_pos - orbit_mouse_vector;
+
         angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        if(delta_mouse_pos.x < 0) {
+            angle *= -1;
+        }
+
+        if (delta_mouse_pos.x != 0) {
+            transform.position = RotatePointAroundPivot(transform.position, target.position, Quaternion.Euler(0, 0, angle * Time.deltaTime * 5));
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+        //delta_mouse_pos = Vector3.zero;
+        previous_mouse_pos = orbit_mouse_vector;
+    }
+
+    public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle) {
+        return angle * (point - pivot) + pivot;
     }
 }
